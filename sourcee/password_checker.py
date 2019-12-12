0055cefb4777b9710b4bd05cf71331245f41fd5b
tshepojrampai <56165880+tshepojrampai@users.noreply.github.com>
2019-12-12 16:14:28 +0200

    final commit password checker

--- sourcee/password_checker.py
+++ sourcee/password_checker.py
@@ -31,29 +31,27 @@ def password_is_valid(password):
        #Checking for a characters in password 
         elif  not re.search("[!@#$%^&*()_+:;'{}[]|\;'<>?/.,]", password):
             raise Exception('password should have at least one special character')
-	
-	else:
-	    print(password, "is okay")
-	
+        else:
+            return True
+    	
 #Defiining the password_is_ok
 def password_is_ok(password):
     #Asserting the length is greater than 8 characters
     if len(password) <=8:
-	return False
+        return False
         print("password's length smaller than 8 characters")
+    
     else: #Assuring that every other condition inclusive of the first two vallidates password
    
-   	 if not re.search('[a-z]', password):
-		return True 
-    	 else:
-		if not re.search('[A-Z]', password):
-	    		return True
-         	else:
-			if not re.search('[0-9]', password):
-	    			return True
-        		else:
-            			if not re.search("[$&+,:;=?@#|'<>.-^*%!]", password):
-					return True
-                	
-        
-password_is_ok("Hellokitty")
+   	    if not re.search('[a-z]', password):
+   	        return True
+   	    else:
+   	        if not re.search('[A-Z]', password):
+   	            return True
+   	        else:
+   	            if not re.search('[0-9]', password):
+   	                return True
+   	            else:
+   	                if not re.search("[$&+,:;=?@#|'<>.-^*%!]", password):
+   	                    return True
+				        
