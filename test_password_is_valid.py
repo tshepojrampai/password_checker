7aa2334981031d68daf1ba9a5493b1e54104d726
Tshepo Rampai <tshepojrampai@gmail.com>
2020-05-13 01:18:33 +0200

    test for length

--- test_password_is_valid.py
+++ test_password_is_valid.py
@@ -6,3 +6,7 @@ import password_checker as pass_check
 def test_password_exists():
     with pytest.raises(Exception, match = "password should exist"):
         pass_check.password_is_valid("")
+
+def test_password_len():
+    with pytest.raises(Exception, match = "password should be longer than than 8 characters"):
+        pass_check.password_is_valid("Hell1!")
