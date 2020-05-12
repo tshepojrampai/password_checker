4b6dd421ff2be40f11b246942f93f0072082c0bd
Tshepo Rampai <tshepojrampai@gmail.com>
2020-05-13 01:22:08 +0200

    test for lower case

--- test_password_is_valid.py
+++ test_password_is_valid.py
@@ -10,3 +10,7 @@ def test_password_exists():
 def test_password_len():
     with pytest.raises(Exception, match = "password should be longer than than 8 characters"):
         pass_check.password_is_valid("Hell1!")
+
+def test_password_lowcase():
+    with pytest.raises(Exception, match = "password should have at least one lowercase letter"):
+        pass_check.password_is_valid("HELLOKITTY1!")
