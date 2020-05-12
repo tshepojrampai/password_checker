c4dd456b7e0071740ef332ab92fc28a2b14bcf3b
Tshepo Rampai <tshepojrampai@gmail.com>
2020-05-13 01:21:03 +0200

    check if the password is longer than 8 characters

--- password_checker.py
+++ password_checker.py
@@ -8,3 +8,10 @@ def password_is_valid(password):
     #Checking if the password exists
     if len(password) == 0:
         raise Exception ("password should exist")
+    else: # first condition has been met if this gets executed
+        
+        # length of the password
+        if len(password)<=8:
+            raise Exception("password should be longer than than 8 characters")
+	
+	
